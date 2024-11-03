44ef570158e7b169494e27efe21b62f5d23096ec
Davi Wendemberg <daviwendemberg@gmail.com>
2024-11-02 22:02:49 -0300

    Commit inicial

---
+++ Repositories/TransactionRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControleFinanceiro.Models;
+using LiteDB;
+
+namespace ControleFinanceiro.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly LiteDatabase _dataBase;
+        private readonly string collectionName = "transactions";
+        public TransactionRepository()
+        {
+            _dataBase = new LiteDatabase("Filename=C:/users/AppData/database.db; Connection=Shared");
+        }
+
+        public List<Transaction> GetAll()
+        {
+            return _dataBase.GetCollection<Transaction>(collectionName)
+                .Query()
+                .OrderByDescending(a=>a.Date)
+                .ToList();
+        }
+        public void Add(Transaction transaction) 
+        {
+            _dataBase.GetCollection<Transaction>(collectionName)
+                .Insert(transaction);
+        }
+        public void Update(Transaction transaction) 
+        {
+            _dataBase.GetCollection<Transaction>(collectionName)
+                .Update(transaction);
+        }
+        public void Delete(Transaction transaction) 
+        {
+            _dataBase.GetCollection<Transaction>(collectionName)
+                .Delete(transaction.Id);
+        }
+    }
+}
